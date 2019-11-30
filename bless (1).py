cd67f679c706f3e6b766c6a29ed196ed21b4e74b
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2019-11-30 18:19:45 +0800

    Update bless (1).py

--- bless (1).py
+++ bless (1).py
@@ -1,11 +1,1 @@
-import datetime
-import time
-count=0
-while True:
-    count=count+1
-    bless='''
-    祝LWD&ZWT考试成绩提高
-    祝LWD&ZWT中考顺利
-    祝LWD&ZWT中考成绩提高'''
-    print("["+time.asctime(time.localtime(time.time()))+" 第"+str(count)+"次]"+bless)
-    time.sleep(2)
+Temporary Unavailable
