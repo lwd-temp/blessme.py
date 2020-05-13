448d197386d5937fffc7a11cea9e00c25058295a
Mikoto <26019675+lwd-temp@users.noreply.github.com>
2020-05-13 15:48:06 +0800

    Delete bless.py

--- bless.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import time
-count=0
-while True:
-    count=count+1
-    bless='''
-    祝LWD考试成绩提高'''
-    print("["+time.asctime(time.localtime(time.time()))+" 第"+str(count)+"次]"+bless)
-    time.sleep(2)
